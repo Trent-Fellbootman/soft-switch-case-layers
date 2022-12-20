724ad7c4a017f288bf815212ab3fb7418283763a
Trent Fellbootman <2713730481@qq.com>
2022-12-20 12:52:20 -0800

    ssc is running

--- layers.py
+++ layers.py
@@ -11,18 +11,19 @@ class ConvSSC(nn.Module):
     strides: int = 1
     padding: str = 'SAME'
     
-    def __init__(self, *args, **kwargs):
-        self.case_matcher = nn.Conv(self.case_count, self.kernel_size, strides=self.strides, padding=self.padding,
-                                    name='case_matcher', *args, **kwargs)
-        self.cases = [nn.Conv(self.features, self.kernel_size, strides=self.strides, padding=self.padding,
-                              name=f'case_{i}', *args, **kwargs) for i in range(self.case_count)]
-        
+    @nn.compact
     def __call__(self, x_batch):
-        case_scores = self.case_matcher(x_batch)
+        case_matcher = nn.Conv(self.case_count, self.kernel_size, strides=self.strides, padding=self.padding,
+                                    name='case_matcher')
+        cases = [nn.Conv(self.features, self.kernel_size, strides=self.strides, padding=self.padding,
+                              name=f'case_{i}') for i in range(self.case_count)]
+        
+        case_scores = case_matcher(x_batch)
         case_vals = ein.rearrange(
-            jnp.array([case(x_batch) for case in self.cases]),
+            jnp.array([case(x_batch) for case in cases]),
             's b h w c -> b h w s c'
         )
+        
         case_weights = nn.softmax(case_scores, axis=-1) # TODO: check which axis is the pixel axis
         weighted_vals = ein.repeat(case_weights, 'b h w s -> b h w s features', features=self.features) * case_vals
         ret_vals = ein.reduce(weighted_vals, 'b h w s c -> b h w c', 'sum')
